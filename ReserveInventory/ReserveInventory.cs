e453da68d58f790aa111a315579da5e52f6ccccf
JMcCarthy <uvatmvf@gmail.com>
2026-07-27 11:21:20 -0600

    update to .net 10

---
+++ ReserveInventory/ReserveInventory.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace ReserveInventory;
+
+public class ReserveInventory
+{
+    private readonly ILogger<ReserveInventory> _logger;
+
+    public ReserveInventory(ILogger<ReserveInventory> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Function1")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
