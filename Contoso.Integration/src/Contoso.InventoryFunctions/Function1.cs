e3ddee1d8474320c01add2d1702c227f5761a129
JMcCarthy <uvatmvf@gmail.com>
2026-07-27 11:12:47 -0600

    update .net 10

--- Contoso.Integration/src/Contoso.InventoryFunctions/Function1.cs
+++ Contoso.Integration/src/Contoso.InventoryFunctions/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Contoso.Integration
+namespace Contoso.InventoryFunctions.src.Contoso.InventoryFunctions
 {
     public static class Function1
     {
